2759eaa7b4f807d249f971664c3c6c747fe650de
manonramel <manon99.ramel@sfr.fr>
2022-11-25 15:56:21 +0100

    FoF code

--- friendly/matchers/fof.py
+++ friendly/matchers/fof.py
@@ -3,13 +3,14 @@ from .. import Matcher
 import FoFCatalogMatching
 from ..utils import Group
 import numpy as np
+import pandas as pd
 
 class FoF(Matcher):     
     def __init__(self, tune_params: dict):
         self.linking_length = tune_params.get('linking_length', 1.)
         
     def __call__(self, cat1, cat2, *args) -> List[Group]:
-        results = FoFCatalogMatching.match(catalog_dict={'cat1':cat1, 'cat2':cat2}, linking_lengths=self.linking_length)
+        results = (FoFCatalogMatching.match(catalog_dict={'cat1':cat1, 'cat2':cat2}, linking_lengths=self.linking_length)).to_pandas()
         results['is_cat1'] = (results['catalog_key']=='cat1')
         res = results.drop('catalog_key', axis=1)
 
